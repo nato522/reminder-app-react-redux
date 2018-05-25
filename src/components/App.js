32476a5c755e3e2e2887943936224b51aa181c52
Renato PL <renapl90@gmail.com>
2018-05-25 18:07:49 +0200

    main structure of component

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,18 @@ import React, { Component } from 'react';
 class App extends Component {
     render(){
         return(
-            <div>App</div>
+            <div className="App">
+                <div className="title">
+                    Reminder App
+                </div>
+                <div className="form-inline">
+                    <div className="from-group">
+                        <input type="text" placeholder="I have to..." className="form-control"/>
+                    </div>
+                    <button type="button" className="btn btn-success">Add Reminder</button>
+
+                </div>
+            </div>
         )
     }
 }
