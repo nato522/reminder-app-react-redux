5a3d9e14dc176bc487eaaee7a5c5208d7863d8db
Renato PL <renapl90@gmail.com>
2018-05-25 18:31:57 +0200

    input working before continuing with redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            text: ''
+        }
+    }
+
+    addReminder(){
+        console.log('state', this.state);
+    }
+
     render(){
         return(
             <div className="App">
@@ -9,9 +20,20 @@ class App extends Component {
                 </div>
                 <div className="form-inline">
                     <div className="from-group">
-                        <input type="text" placeholder="I have to..." className="form-control"/>
+                        <input
+                            type="text"
+                            placeholder="I have to..."
+                            className="form-control"
+                            onChange={event => this.setState({text: event.target.value})}
+                        />
                     </div>
-                    <button type="button" className="btn btn-success">Add Reminder</button>
+                    <button
+                        type="button"
+                        className="btn btn-success"
+                        onClick={() => this.addReminder()}
+                    >
+                        Add Reminder
+                    </button>
 
                 </div>
             </div>
