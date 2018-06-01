af2941de1e4f1e9c22c637dabad9d1cb3efd2756
Renato PL <renapl90@gmail.com>
2018-06-01 19:45:28 +0200

    adding deleteReminder

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,19 @@
-import { ADD_REMINDER } from '../constants';
+import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
 
 export const addReminder = (text) => {
     const action = {
         type: ADD_REMINDER,
         text
     }
-    console.log(action);
+    console.log('addReminder',action);
+    return action;
+}
+
+export const deleteReminder = (id) => {
+    const action = {
+        type: DELETE_REMINDER,
+        id
+    }
+    console.log('deleteReminder',action);
     return action;
 }
